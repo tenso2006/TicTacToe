3257f404058ae099f440b93a7c820025a18592d7
Tenzin Sonam <tenso2006@gmail.com>
2017-03-13 11:15:54 -0700

    Add: prompt module and board setting

---
+++ game.js
@@ -0,0 +1,22 @@
+var prompt = require('prompt');
+
+prompt.start();
+
+var board = {
+  1: ' ',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: ' ',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: ' '
+};
+
+prompt.get(['username', 'email'], function (err, result) {
+  // Log the results.
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+  console.log('  email: ' + result.email);
+});

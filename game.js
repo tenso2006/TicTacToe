c9a0d75bf116e8f095b18740697e799a044e6c58
Tenzin Sonam <tenso2006@gmail.com>
2017-03-13 11:52:33 -0700

    Add: test modules and show board function

--- game.js
+++ game.js
@@ -27,5 +27,12 @@ var drawOnBoard = function (location, draw) {
 };
 
 
-
+var showBoard = function () {
+  console.log('\n' +
+    ' ' + board[1] + ' : ' + board[2] + ' : ' + board[3] + '\n' +
+    ' ---------\n' +
+    ' ' + board[4] + ' : ' + board[5] + ' : ' + board[6] + '\n' +
+    ' ---------\n' +
+    ' ' + board[7] + ' : ' + board[8] + ' : ' + board[9] + '\n');
+};
 
